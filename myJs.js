e9b9572a8aac7395fbeb04ddc27170187d0b03a8
Thommy Baez <thomasbbtrain2@gmail.com>
2023-10-10 15:53:33 -0400

    Beta of the application Finished adding input for weather temperature and weather code for knowing the general weather (ie. sunny, couldy...) just needs further input testing and some css changes to make it look better.

--- myJs.js
+++ myJs.js
@@ -3,14 +3,10 @@ let weatherUrl = `https://api.open-meteo.com/v1/gfs?`
 
 let searchBar = document.querySelector(".searchBar");
 let timeInput = document.querySelector('#timeInput');
-
-let locationArr = [];
-let date = new Date();
-let addDropVis = true;
-
+let weatherImg = document.querySelector('#weatherImg');
 let dateSelector = document.querySelector('#dateSelection')
 
-console.log(date);
+let date = new Date();
 
 async function getUSLocation(){
     let url = locationUrl + searchBar.value + "&count=8";
@@ -31,7 +27,7 @@ async function getWeatherOfLocation(usArray)
     let lat = usArray[0].latitude;
     let lon = usArray[0].longitude;
 
-    fullUrl = weatherUrl + `latitude=${lat}&longitude=${lon}&hourly=temperature_2m,weathercode&forecast_days=7&temperature_unit=fahrenheit&windspeed_unit=mph`
+    fullUrl = weatherUrl + `latitude=${lat}&longitude=${lon}&hourly=temperature_2m,weathercode&forecast_days=16&temperature_unit=fahrenheit&windspeed_unit=mph`
 
     let weatherData = await fetch(fullUrl);
     let jsonWeatherData = await weatherData.json();
@@ -69,47 +65,99 @@ function getDisplayWeatherData(weatherData){
 
     let indexOfValues = []
     let index = weatherData.hourly.time.indexOf(timeString);
-    indexOfValues.append(index);
-    indexOfValues.append(index +1);
-    indexOfValues.append(index +3);
+    indexOfValues.push(index);
+    indexOfValues.push(index +1);
+    indexOfValues.push(index +3);
 
-    let averageTemp = averageTemp();
-    let averageWeather = averageWeather();
+    let averageTemp = function (indexArray, weatherData){
+        let sumOfTemps = 0;
+        for(let i = 0; i < indexArray.length; i++)
+        {
+            sumOfTemps += weatherData.hourly.temperature_2m[indexArray[i]];
+        }
+    
+        return sumOfTemps/indexArray.length;
+    }
 
-    document.querySelector('#tempDisplay').innerHTML += averageTemp();
+    let averageWeather = function(indexArray, weatherData){
+        let maxCode = -1;
+        for(let i = 0; i < indexArray.length; i++)
+        {
+            maxCode = Math.max(maxCode, weatherData.hourly.weathercode[indexArray[i]]);
+        }
+        return maxCode;
+    }
+
+    let maxCode = averageWeather(indexOfValues,weatherData)
+    let weatherText = document.querySelector('#weatherDisplay');
+
+    console.log(maxCode < 20);
+
+    if(maxCode < 20)
+    {
+        weatherImg.setAttribute('src','images/sunny.png');
+        weatherText.innerHTML += 'Sunny';
+    }
+    else if (maxCode >= 20 && maxCode < 49)
+    {
+        weatherImg.setAttribute('src','images/part-cloudy.png');
+        weatherText.innerHTML += 'Patialy Cloudy';
+    }
+    else if (maxCode >= 50 && maxCode < 60)
+    {
+        weatherImg.setAttribute('src','images/part-rain.png');
+        weatherText.innerHTML += 'Patialy Cloudy';
+    }
+    else if (maxCode >= 60 && maxCode < 66)
+    {
+        weatherImg.setAttribute('src','images/rain.png');
+        weatherText.innerHTML += 'Rain';
+    }
+    else if (maxCode >= 66 && maxCode < 80)
+    {
+        weatherImg.setAttribute('src','images/snow.png');
+        weatherText.innerHTML += 'Snow';
+    }
+    else
+    {
+        weatherImg.setAttribute('src','images/thunder.png');
+        weatherText.innerHTML += 'Thunder';
+    }
+
+    document.querySelector('#tempDisplay').innerHTML += Math.round(averageTemp(indexOfValues, weatherData)) + '&degF';
 }
-
-function averageTemp(){
-
-}
-
-function averageWeather(){
-
-}
-
 
 function enter(e){
     if(e.key == "Enter")
     {
+        let errorMessage = document.querySelector('#errorMessage');
+        let informationDIsplay =  document.querySelector('.informationDisplay');
+
         getUSLocation().then( function(val)
         {
-            if(!addDropVis && !Boolean(dateSelector.value))
+            if(!Boolean(dateSelector.value))
             {
-                document.querySelector('#errorMessage').style.display = 'block';
-                document.querySelector('#errorMessage').innerHTML = 'You need to add a valid date'
-                document.querySelector('.informationDisplay').style.display ='none';
+                errorMessage.style.display = 'block';
+                errorMessage.innerHTML = 'You need to add a valid <span style="color:rgba(253, 29, 29, 0.871)">DATE</span>';
+                informationDIsplay.style.display ='none';
             }
             else if(!document.querySelector('#timeInput').value)
             {
-                document.querySelector('#errorMessage').style.display = 'block';
-                document.querySelector('#errorMessage').innerHTML = 'You need to add a valid time';
-                document.querySelector('.informationDisplay').style.display ='none';
+                errorMessage.style.display = 'block';
+                errorMessage.innerHTML = 'You need to add a valid <span style="color:rgba(253, 29, 29, 0.871)">TIME</span>';
+                informationDIsplay.style.display ='none';
             }
             else{
-                document.querySelector('#errorMessage').style.display = 'none';
-                document.querySelector('.informationDisplay').style.display ='flex';
+                errorMessage.style.display = 'none';
+                informationDIsplay.style.display ='flex';
                 getWeatherOfLocation(val); 
             }
+        }).catch(function (e)
+        {
+            errorMessage.style.display = 'block';
+            informationDIsplay.style.display ='none';
+            errorMessage.innerHTML = 'You need to add a valid <span style="color:rgba(253, 29, 29, 0.871)">CITY</span>';
+            return;
         });
     }
 }
